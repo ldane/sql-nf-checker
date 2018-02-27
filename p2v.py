85a323bf72efd6006c04afe467d5dd376d1a9978
Levent Dane <limon@verj.us>
2018-02-27 16:52:02 -0600

    Typo #5

--- p2v.py
+++ p2v.py
@@ -105,14 +105,14 @@ def check_2nf(my_table, my_cursor):
     from itertools import combinations
     n = len(my_table.key_list)
     for nonkey in my_table.nonkey_list:
-        for i in range(n-1):
+        for i in range(1,n-1):
             for test_case in combinations(my_table.key_list, i):
                 test_str=''.join(['%d,' %(j) for j in test_case])[:-1]
                 query = 'SELECT COUNT(*) FROM ' + \
                         '(SELECT %s, COUNT(DISTINCT %s) ' % (test_str, nonkey) + \
                         'as c FROM %s ' %(my_table.table_name) + \
-                        'WHERE %s is NOT NULL' %(nonkey) + \
-                        'GROUP BY %s) as t' %(test_str) + \
+                        'WHERE %s is NOT NULL ' %(nonkey) + \
+                        'GROUP BY %s) as t ' %(test_str) + \
                         'WHERE c!=1;'
                 execute_statement(my_cursor, query)
                 result_data = my_cursor.fetchall()
