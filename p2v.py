3ae29cd2967bb3f2ac265a3ae654d6ef8bf9cb80
Levent Dane <limon@verj.us>
2018-02-27 01:17:03 -0600

    Fix whitespace and add shebang

--- p2v.py
+++ p2v.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import vertica_python
 import sys
 
@@ -5,7 +7,7 @@ class Table:
 	table_name = ""
 	key_list = list()
 	nonkey_list = list()
-	
+
 	def __init__(self, table_schema):
 		#find table name 
 		table_name_index = table_schema.find('(')
@@ -16,10 +18,10 @@ class Table:
 		first = table_schema.find('(')
 		last = table_schema.rfind(')')
 		final_string = table_schema[first+1:last]
-		
+
 		# assigned table_name
 		self.table_name = table_schema[:table_name_index]
-		
+
 		# split by ,
 		columns = final_string.split(',')
 		self.key_list = list()
@@ -30,17 +32,17 @@ class Table:
 				self.key_list.append(col_stripped.replace('(k)', ''))
 			else:
 				self.nonkey_list.append(col_stripped)
-				
+
 	# check for anomalies in names 
 	def check_name_validity(self):
 		#return true if all names valid table/column names
 		#returns false if table breaks some standard format
 		return True
-		
+
 def check_nf(my_table, my_cursor):
 	#returns a list of booleans representing normal form checks and a string describing the reason for failure
 	#[1nf, 2nf, 3nf, bcnf], 'reason'
-	
+
 	is_1nf, is_2nf, is_3nf, is_bcnf = False, False, False, False
 	reason = ''
 	is_1nf, reason = check_1nf(my_table, my_cursor)
@@ -52,17 +54,17 @@ def check_nf(my_table, my_cursor):
 				is_bcnf, reason = check_bcnf(my_table, my_cursor)
 	#testing
 	return [is_1nf, is_2nf, is_3nf, is_bcnf], reason
-	
+
 def check_1nf(my_table, my_cursor):
 	#returns boolean, string
-	
+
 	#check if any null values in the supposed primary key columns (composite)
 	return False, ''
 
 def check_2nf(my_table, my_cursor):
 	#returns boolean, string
 	return False, ''
-	
+
 def check_3nf(my_table, my_cursor):
 	#returns boolean, string
 	return False, ''
@@ -70,19 +72,19 @@ def check_3nf(my_table, my_cursor):
 def check_bcnf(my_table, my_cursor):
 	#returns boolean, string
 	return False, ''
-		
+
 def execute_statement(my_cursor, my_statement):
 	# executes sql statement and writes to file
 	my_cursor.execute(my_statement)
-	
+
 	# before writing to file, separate the statement's WHERE JOIN GROUP clause.
 	statement1 = my_statement.replace('WHERE', '\nWHERE')
 	statement2 = statement1.replace('GROUP', '\nGROUP')
 	statement3 = statement2.replace('INNER JOIN', '\nINNER JOIN')
-	
+
 	with open ('NF.sql', 'a') as f_sql:
 		f_sql.write(statement3 + '\n\n')
-		
+
 def print_row(my_table_name, nf_boolean_list, my_reason):
 	#first print table name, then print which NF fails if any, if there is a failure then the myReason string is not empty
 	failed = 'PASSED'
@@ -96,9 +98,9 @@ def print_row(my_table_name, nf_boolean_list, my_reason):
 		failed = 'BCNF'
 	if (len(my_table_name) < 8):
 		print(my_table_name + '\t\t' + failed + '\t\t' + my_reason)
-	else: 
+	else:
 		print(my_table_name + '\t' + failed + '\t\t' + my_reason)
-		
+
 def main():
 	# file login.ini contains host, username, password, and db name
 	with open('login.ini', 'r') as f:
@@ -108,30 +110,30 @@ def main():
 		database = f.readline().strip()
 
 	conn_info = {'host': host,
-				 'port': 5433,
-				 'user': username,
-				 'password': password,
-				 'database': database,
-				 'read_timeout': 600,
-				 'connection_timeout': 5}
+                     'port': 5433,
+                     'user': username,
+                     'password': password,
+                     'database': database,
+                     'read_timeout': 600,
+                     'connection_timeout': 5}
 
-				 
+
 	# clear the log files
 	open ('NF.sql', 'w').close()
-	
+
 	# grab input from command line argument
 	# only 1 argument allowed
 	if len(sys.argv) != 2:
 		return
 	# argument should be name of text file that is input 
 	lines = [line.rstrip('\n') for line in open(sys.argv[1])]
-	
+
 	# connect to database
 	connection = vertica_python.connect(**conn_info)
 	cur = connection.cursor()
-	
+
 	print('#Table\t\tFailed\t\tReason')
-	
+
 	# from the schema, evaluate each line into the table class which forms a key and non key list
 	for line in lines:
 		temp_table = Table(line)
@@ -147,8 +149,10 @@ def main():
 	#cur.execute(stm)
 	execute_statement(cur, stm)
 
-	for row in cur.iterate():
+        for row in cur.iterate():
 		print(row)
-	
+
 if __name__ == "__main__":
 	main()
+
+# vim: ts=4 sw=4 et nowrap 
