986c61add3713769c5e304bef5e9819c4166591e
victorlcuh <31748109+victorlcuh@users.noreply.github.com>
2018-02-26 20:14:05 -0600

    Adding input.txt and p2v.py
    input.txt contains the schema for the database
    
    p2v.py is the python code that generates sql queries to check normal forms

---
+++ p2v.py
@@ -0,0 +1,154 @@
+import vertica_python
+import sys
+
+class Table:
+	table_name = ""
+	key_list = list()
+	nonkey_list = list()
+	
+	def __init__(self, table_schema):
+		#find table name 
+		table_name_index = table_schema.find('(')
+		# if the schema is wrong and has no ( 
+		if table_name_index == -1:
+			return
+		rest_of_string = table_schema[table_name_index:]
+		first = table_schema.find('(')
+		last = table_schema.rfind(')')
+		final_string = table_schema[first+1:last]
+		
+		# assigned table_name
+		self.table_name = table_schema[:table_name_index]
+		
+		# split by ,
+		columns = final_string.split(',')
+		self.key_list = list()
+		self.nonkey_list = list()
+		for col in columns:
+			col_stripped = col.strip()
+			if '(k)' in col_stripped:
+				self.key_list.append(col_stripped.replace('(k)', ''))
+			else:
+				self.nonkey_list.append(col_stripped)
+				
+	# check for anomalies in names 
+	def check_name_validity(self):
+		#return true if all names valid table/column names
+		#returns false if table breaks some standard format
+		return True
+		
+def check_nf(my_table, my_cursor):
+	#returns a list of booleans representing normal form checks and a string describing the reason for failure
+	#[1nf, 2nf, 3nf, bcnf], 'reason'
+	
+	is_1nf, is_2nf, is_3nf, is_bcnf = False, False, False, False
+	reason = ''
+	is_1nf, reason = check_1nf(my_table, my_cursor)
+	if is_1nf:
+		is_2nf, reason = check_2nf(my_table, my_cursor)
+		if is_2nf:
+			is_3nf, reason = check_3nf(my_table, my_cursor)
+			if is_3nf:
+				is_bcnf, reason = check_bcnf(my_table, my_cursor)
+	#testing
+	return [is_1nf, is_2nf, is_3nf, is_bcnf], reason
+	
+def check_1nf(my_table, my_cursor):
+	#returns boolean, string
+	
+	#check if any null values in the supposed primary key columns (composite)
+	return False, ''
+
+def check_2nf(my_table, my_cursor):
+	#returns boolean, string
+	return False, ''
+	
+def check_3nf(my_table, my_cursor):
+	#returns boolean, string
+	return False, ''
+
+def check_bcnf(my_table, my_cursor):
+	#returns boolean, string
+	return False, ''
+		
+def execute_statement(my_cursor, my_statement):
+	# executes sql statement and writes to file
+	my_cursor.execute(my_statement)
+	
+	# before writing to file, separate the statement's WHERE JOIN GROUP clause.
+	statement1 = my_statement.replace('WHERE', '\nWHERE')
+	statement2 = statement1.replace('GROUP', '\nGROUP')
+	statement3 = statement2.replace('INNER JOIN', '\nINNER JOIN')
+	
+	with open ('NF.sql', 'a') as f_sql:
+		f_sql.write(statement3 + '\n\n')
+		
+def print_row(my_table_name, nf_boolean_list, my_reason):
+	#first print table name, then print which NF fails if any, if there is a failure then the myReason string is not empty
+	failed = 'PASSED'
+	if nf_boolean_list[0] == False:
+		failed = '1NF'
+	elif nf_boolean_list[1] == False:
+		failed = '2NF'
+	elif nf_boolean_list[2] == False:
+		failed = '3NF'
+	elif nf_boolean_list[3] == False:
+		failed = 'BCNF'
+	if (len(my_table_name) < 8):
+		print(my_table_name + '\t\t' + failed + '\t\t' + my_reason)
+	else: 
+		print(my_table_name + '\t' + failed + '\t\t' + my_reason)
+		
+def main():
+	# file login.ini contains host, username, password, and db name
+	with open('login.ini', 'r') as f:
+		host = f.readline().strip()
+		username = f.readline().strip()
+		password = f.readline().strip()
+		database = f.readline().strip()
+
+	conn_info = {'host': host,
+				 'port': 5433,
+				 'user': username,
+				 'password': password,
+				 'database': database,
+				 'read_timeout': 600,
+				 'connection_timeout': 5}
+
+				 
+	# clear the log files
+	open ('NF.sql', 'w').close()
+	
+	# grab input from command line argument
+	# only 1 argument allowed
+	if len(sys.argv) != 2:
+		return
+	# argument should be name of text file that is input 
+	lines = [line.rstrip('\n') for line in open(sys.argv[1])]
+	
+	# connect to database
+	connection = vertica_python.connect(**conn_info)
+	cur = connection.cursor()
+	
+	print('#Table\t\tFailed\t\tReason')
+	
+	# from the schema, evaluate each line into the table class which forms a key and non key list
+	for line in lines:
+		temp_table = Table(line)
+		if (temp_table.check_name_validity()):
+			#table names are valid, now check normal form
+			normal_forms, reason = check_nf(temp_table, cur)
+			print_row(temp_table.table_name, normal_forms, reason)
+
+	#dataset = 'Employees'
+
+	#stm = 'SELECT * FROM ' + dataset
+
+	#cur.execute(stm)
+	#executeStatement(cur, stm)
+
+	#for row in cur.iterate():
+		#print(row)
+	
+if __name__ == "__main__":
+	main()
